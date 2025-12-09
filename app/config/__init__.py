e3aff91356b55f8be2d677ef1824d580ec64c109
gorspeed06 <gourav.singal@project44.com>
2025-12-09 09:43:11 +0530

    Ranking agent

---
+++ app/config/__init__.py
@@ -0,0 +1,8 @@
+"""
+Configuration module for storing embeddings and other constants.
+"""
+
+from app.config.beauty_standard import get_beauty_standard_embedding
+
+__all__ = ["get_beauty_standard_embedding"]
+
---
+++ app/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utility functions module.
+"""
+
+from app.utils.similarity import compute_cosine_similarity
+
+__all__ = ["compute_cosine_similarity"]
+
